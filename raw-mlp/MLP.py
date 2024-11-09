49f32f096831875ecf4e0399eab2574afe407928
MichaelGurevich <105605801+MichaelGurevich@users.noreply.github.com>
2024-11-09 21:33:41 +0200

    Update MLP.py

--- raw-mlp/MLP.py
+++ raw-mlp/MLP.py
@@ -2,8 +2,6 @@ from Matrix import Matrix
 import random
 import math
 
-INPUT_NUM = 2
-
 def add_bias(dot_mat: Matrix, bias_v: Matrix) -> Matrix:
         z = dot_mat
         for i in range(z.n_cols):
@@ -21,84 +19,79 @@ def convert_labels(y: Matrix):
         one_hot = [0 for _ in range(10)]
         one_hot[y.matrix[i][0]] = 1
         one_hot_data.matrix[i] = one_hot
+    return one_hot_data
 
 
 class MLP:
-    def __init__(self, hidn_lay_num, hidn_node_num, ouput_num, l_rate):
+    def __init__(self,n_features, hidn_lay_num, hidn_node_num, ouput_num, l_rate=0.1):
         self.hidn_lay_num = hidn_lay_num
         self.hidn_node_num = hidn_node_num
         self.output_num = ouput_num
-        self.weights = []
-        self.bias = []
         self.l_rate = l_rate
+        self.initialize_weights_bias(n_features)
+
+
+    def init_dim_list(self, n_features):
+        self.dim_list = []
+        self.dim_list.append((self.hidn_node_num, n_features))
+        for i in range(1, self.hidn_lay_num):
+            self.dim_list.append((self.hidn_node_num, self.hidn_node_num))
+        self.dim_list.append((self.output_num, self.hidn_node_num))
+
+    
+    def initialize_weights_bias(self, n_features):
+        self.init_dim_list(n_features)
+
+        self.w_ = []
+        self.b_ = []
+
+        for i in range(len(self.dim_list)):
+            w = Matrix(self.dim_list[i][0], self.dim_list[i][1])
+            b = Matrix(self.dim_list[i][0],1)
+            w.element_oper(lambda x : random.uniform(-1, 1))
+            b.element_oper(lambda x : random.uniform(-1, 1))
+            self.w_.append(w)
+            self.b_.append(b)
+
+    def forward(self, X, y):
         self.a_ = []
-
-        self.initialize_weights_bias(layer_type=0) # initializing the first hidden layer
-        for _ in range(self.hidn_lay_num-1): # initializing every other hidden layer
-            self.initialize_weights_bias(layer_type=1)
-        self.initialize_weights_bias(layer_type=2) # initializing the output layer
-
-     # Initilize the weights and bias of the first hidden layer
-    def initialize_weights_bias(self, layer_type=1):
-        if layer_type == 0: # first layer
-            layer_weights = Matrix(self.hidn_node_num, INPUT_NUM)
-            layer_bias = Matrix(self.hidn_node_num, 1)
-        elif layer_type == 1: # any other hidden layer
-            layer_weights = Matrix(self.hidn_node_num, self.hidn_node_num)
-            layer_bias = Matrix(self.hidn_node_num, 1)
-        else: # output layer
-            layer_weights = Matrix(self.output_num, self.hidn_node_num)
-            layer_bias = Matrix(self.output_num, 1)
-            
-        layer_weights.element_oper(lambda element : random.uniform(-1, 1))
-        self.weights.append(layer_weights)
-                
-        layer_bias.element_oper(lambda element : random.uniform(-1, 1))
-        self.bias.append(layer_bias)
-
-
-    def feed_forward(self, X):
-        
-        dot_mat = Matrix.dot(X, self.weights[0].mat_t(return_mat=True))
-        z = add_bias(dot_mat, self.bias[0])
+        dot_mat = Matrix.dot(X, self.w_[0].mat_t(return_mat=True))
+        z = add_bias(dot_mat, self.b_[0])
         z.element_oper(sigmoid)
         self.a_.append(z)
 
-        for i in range(1, len(self.weights)):
-            dot_mat = Matrix.dot(z, self.weights[i].mat_t(return_mat=True))
-            z = add_bias(dot_mat, self.bias[i])
+        for i in range(1, len(self.w_)):
+            dot_mat = Matrix.dot(z, self.w_[i].mat_t(return_mat=True))
+            z = add_bias(dot_mat, self.b_[i])
             z.element_oper(sigmoid)
             self.a_.append(z)
 
-        # TODO: calculate the cost for each neuron
-        cost_mat = Matrix(1, 1)
+        labels = convert_labels(y)
+        cost = 0
+        cost_mat = Matrix(z.n_cols, 1)
+        for j in range(z.n_cols):
+            for i in range(z.n_rows):
+                cost += z.matrix[i][j] - labels.matrix[i][j]
 
+            cost_mat.matrix[j][0] = cost / z.n_rows
+            
         return cost_mat
     
     def backwards_propagation(self, cost_mat: Matrix, y):
 
         '''
-        z = sigma(x_i * w_i) + b
-
-        L = sigma(y - a)^2
-
-        
-        
-        for each weight of each output neuron:
-        w = w - l_rate * (dL/dw) 
-        dL/dw = dz/dw * da/dz * dL/da
-        dz/dw = a_h
-        da/dz = f'(z) = (1/1+e^(-z)) * (1 - 1/1+e^(-z)) 
-        dL/da = 2(a-y)
-
-        update to the weights of the output layer
-        dL/dw = a(of the prev layer) * (1/1+e^(-z)) * (1 - 1/1+e^(-z)) * 2(a-y)
-
-
+        Part 1:
+        - Get the gradient for the w_ and b_for the output layer 
+        d_L__d_a 
+        d_a__d_z
+        d_z__d_w
         '''
 
+        d_L__d_a = Matrix(1, 1)
+        d_a__d_z = Matrix.element_oper_return(self.a_, lambda a : a * (1 - a))
 
-        # getting the gradient for updating the output weights
+
+        # getting the gradient for updating the output w_
         
         
        
