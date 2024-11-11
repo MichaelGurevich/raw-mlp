5c3c2378b95e3d2bba791602b61538a28f4c4d49
MichaelGurevich <105605801+MichaelGurevich@users.noreply.github.com>
2024-11-11 21:44:04 +0200

    Update Matrix.py

--- raw-mlp/Matrix.py
+++ raw-mlp/Matrix.py
@@ -1,109 +1,106 @@
+import random
+
 class Matrix:
-    def __init__(self, n_rows, n_cols):
-        self.n_rows = n_rows
-        self.n_cols = n_cols
-        self.matrix = [[0 for i in range(n_cols)] for j in range(n_rows)]
-
-    def add_scalar(self, scalar):
-        for i in range(self.n_rows):
-            for j in range(self.n_cols):
-                self.matrix[i][j] += scalar
-
-
-    def mult_by_scalar(self, scalar):
-        for i in range(self.n_rows):
-            for j in range(self.n_cols):
-                self.matrix[i][j] *= scalar
-    
-    def dot(self, mat):
-        if self.n_cols != mat.n_rows:
-            raise Exception(f"cannot dot {self.n_rows} x {self.n_cols} * {mat.n_rows} x {mat.n_cols}" )
+    def __init__(self, rows, cols, **kwargs ):
+        self.rows = rows
+        self.cols = cols
+        if "data" in kwargs:
+            self.matrix = kwargs["data"]
         else:
-            dot_mat = Matrix(self.n_rows, mat.n_cols)
-            
-            for i in range(dot_mat.n_rows):
-                for j in range(dot_mat.n_cols):
-                    sum = 0
-                    for k in range(self.n_cols):
-                        sum += self.matrix[i][k] * mat.matrix[k][j]
+            self.zeros()
 
-                    dot_mat.matrix[i][j] = sum
-        
-        return dot_mat
-    
 
-    def dot(mat1, mat2):
-        if mat1.n_cols != mat2.n_rows:
-            raise Exception(f"cannot dot {mat1.n_rows} x {mat1.n_cols} * {mat2.n_rows} x {mat2.n_cols}" )
-         
-        else:
-            dot_mat = Matrix(mat1.n_rows, mat2.n_cols)
-            
-            for i in range(dot_mat.n_rows):
-                for j in range(dot_mat.n_cols):
-                    sum = 0
-                    for k in range(mat1.n_cols):
-                        sum += mat1.matrix[i][k] * mat2.matrix[k][j]
-
-                    dot_mat.matrix[i][j] = sum
-        
-        return dot_mat
-    
-
-    def mat_add(self, mat):
-        if self.n_rows != mat.n_rows or self.n_cols != mat.n_cols:
-            raise Exception("Matrices are not the same dimentions")
-        else:
-            for i in range(self.n_rows):
-                for j in range(self.n_cols):
-                    self.matrix[i][j] += mat.matrix[i][j]
-
-    
-    '''def mat_add(mat1, mat2):
-        if mat1.n_rows != mat2.n_rows or mat1.n_cols != mat2.n_cols:
-            raise Exception("Matrices are not the same dimentions")
-        else:
-            add_mat = Matrix(mat1.n_rows, mat2.n_cols)
-            for i in range(mat1.n_rows):
-                for j in range(mat1.n_cols):
-                    add_mat[i][j] += mat1.matrix[i][j] + mat2.matrix[i][j]
-
-        return add_mat
-        '''
-    
-
-    def mat_t(self, return_mat=False):
-        mat_t = Matrix(self.n_cols, self.n_rows)
-
-        for i in range(mat_t.n_rows):
-            for j in range(mat_t.n_cols):
-                mat_t.matrix[i][j] = self.matrix[j][i]
-
-        if return_mat == True:
-            return mat_t
-        
-        self.n_cols = mat_t.n_cols
-        self.n_rows = mat_t.n_rows
-        self.matrix = mat_t.matrix
-
-    def element_oper(self, func):
-
-        for i in range(self.n_rows):
-            for j in range(self.n_cols):
-                self.matrix[i][j] = func(self.matrix[i][j])
-        
-            
-        
-    def element_oper_return(mat, func):
-        new_mat = Matrix(mat.n_rows, mat.n_cols)
-        for i in range(mat.n_rows):
-            for j in range(mat.n_cols):
-                new_mat.matrix[i][j] = func(mat.matrix[i][j])
-        return new_mat
+    def print_matrix(self):
+        for i in range(self.rows):
+            print(f"{self.matrix[i]}")
 
     def shape(self):
-        print(f'{self.n_rows}x{self.n_cols}')
+        print(f"{self.rows}x{self.cols}")
 
-    def print_mat(self):
-        for i in range(self.n_rows):
-            print(self.matrix[i])
+    def get_element(self, row, col):
+        if row < 0 or col < 0 or row > self.rows-1 or col > self.cols-1:
+            raise Exception("Indexs are out of bounds")
+        return self.matrix[row][col]
+
+    def set_element(self, row, col, new_val):
+        if row < 0 or col < 0 or row > self.rows-1 or col > self.cols-1:
+            raise Exception("Indexs are out of bounds")
+        self.matrix[row][col] = new_val
+    
+    def zeros(self):
+        self.matrix = self.matrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+    
+    def ones(self):
+        self.matrix = self.matrix = [[1 for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def random(self):
+        self.matrix = self.matrix = [[random.uniform(0,1) for _ in range(self.cols)] for _ in range(self.rows)]
+
+    def add(mat1, mat2):
+        if mat1.rows != mat2.rows or mat1.cols != mat2.cols:
+            raise Exception("Matrixes are not the same dimensions")
+
+        output_mat = Matrix(mat1.rows, mat1.cols)
+        for i in range(mat1.rows):
+            for j in range(mat1.cols):
+                output_mat.set_element(i, j, mat1.get_elemet[i][j] + mat2.get_element[i][j])
+            
+        return output_mat
+    
+    def subtract(mat1, mat2):
+        if mat1.rows != mat2.rows or mat1.cols != mat2.cols:
+            raise Exception("Matrixes are not the same dimensions")
+
+        output_mat = Matrix(mat1.rows, mat1.cols)
+        for i in range(mat1.rows):
+            for j in range(mat1.cols):
+                output_mat.set_element(i, j, mat1.get_elemet[i][j] - mat2.get_element[i][j])
+            
+        return output_mat
+
+    def multiply_by_scalar(mat, scalar):
+        output_mat = Matrix(mat.rows, mat.cols)
+
+        for i in range(mat.rows):
+            for j in range(mat.cols):
+                output_mat.set_element(i, j) = mat.get_elemet(i, j) * scalar
+        
+        return output_mat
+
+    def elementwise_multiply(mat1, mat2):
+        if mat1.rows != mat2.rows or mat1.cols != mat2.cols:
+            raise Exception("Matrixes are not the same dimensions")
+
+        output_mat = Matrix(mat1.rows, mat1.cols)
+        for i in range(mat1.rows):
+            for j in range(mat1.cols):
+                output_mat.set_element(i, j, mat1.get_elemet[i][j] * mat2.get_element[i][j])
+            
+        return output_mat
+
+    def multiply(mat1, mat2):
+        if mat1.n_cols != mat2.n_rows:
+            raise ValueError(f"Cannot perform dot product: {mat1.n_rows} x {mat1.n_cols} * {mat2.n_rows} x {mat2.n_cols}")
+
+        # Initialize the result matrix with zeros
+        dot_mat = Matrix(mat1.n_rows, mat2.n_cols)
+
+        # Perform matrix multiplication
+        for i in range(dot_mat.n_rows):
+            for j in range(dot_mat.n_cols):
+                dot_sum = 0
+                for k in range(mat1.n_cols):
+                    dot_sum += mat1.matrix[i][k] * mat2.matrix[k][j]
+
+                dot_mat.matrix[i][j] = dot_sum
+
+        return dot_mat
+    
+    def transpose(mat):
+        transposed_mat = Matrix(mat.cols, mat.rows)
+
+        for i in range(transposed_mat.rows):
+            for j in range(transposed_mat.cols):
+                transposed_mat.set_element(i, j, mat.get_element(j,i))
+
+        return transposed_mat
